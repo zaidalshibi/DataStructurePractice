d56947b0d4b7a4dbd86f00f6c1574f240d163ca0
Zaid alshibi <zaidealshibi@gmail.com>
2022-09-08 23:45:00 +0300

    created Node class and tested it

---
+++ LinkedList/Node.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+module.exports = Node;
+
+// Author: @zaidalshibi
